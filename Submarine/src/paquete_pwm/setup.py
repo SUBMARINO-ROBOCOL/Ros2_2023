dae7f813397dc8ca9ad25563e7432652c532b7d5
tatiperez59 <k.perezs@uniandes.edu.co>
2023-10-20 15:33:07 -0500

    Se agrega nuevo paquete

---
+++ Submarine/src/paquete_pwm/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'paquete_pwm'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tatiana',
+    maintainer_email='k.perezs@uniandes.edu.co',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
